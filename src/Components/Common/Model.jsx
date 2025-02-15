91dbfdd41b217ae68e1f9c7e608343f206542b6b
jihansoft <jihanuddinsoft@gmail.com>
2025-02-16 02:10:28 +0600

    model iss fix

--- src/Components/Common/Model.jsx
+++ src/Components/Common/Model.jsx
@@ -317,8 +317,8 @@ const handleEmailSignIn = async (e) => {
   //   }
   // };
   return (
-    <div className="fixed top-0 max-sm:top-[-12px] left-0 max-sm:left-[-5px] w-full h-full flex items-center justify-center z-50">
-      <div className="relative bg-bgPrimary w-[90%] max-w-2xl md:max-w-4xl rounded-lg shadow-lg p-4 md:p-8 flex flex-col md:flex-row">
+    <div className="fixed top-0 max-sm:top-[-12px] left-[-12px] max-xl:left-[-5px] max-sm:left-[-5px] w-full h-full flex items-center justify-center z-50">
+      <div className="relative bg-bgPrimary w-[80%] max-w-2xl md:max-w-4xl rounded-lg shadow-lg p-4 md:p-8 flex flex-col md:flex-row">
         {/* Close Button */}
         <button
           onClick={() => onClose(false)}
